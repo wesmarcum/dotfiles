e92afa168d17f35158369eb50a0c7ad6f6c1180c
wes <34384551+wesmarcum@users.noreply.github.com>
2023-04-23 17:40:19 -0500

    update fzf options

--- zsh/rc.d/10-fzf.zsh
+++ zsh/rc.d/10-fzf.zsh
@@ -55,7 +55,10 @@ if type fzf > /dev/null 2>&1; then
     fi
 
     # Set default fzf options.
-    export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border --multi"
+    export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border=double --border-label-pos=10 --multi --color='dark,fg+:red'"
+    export FZF_ALT_C_OPTS="--border-label='| Home Directories |' --preview='tree -C {} | head -n 50'"
+    export FZF_CTRL_T_OPTS="--border-label='| Home Files |'"
+    export FZF_CTRL_R_OPTS="--border-label='| History |'"
 
     # Load fzf functions.
     autoload -Uz fzf-rm
