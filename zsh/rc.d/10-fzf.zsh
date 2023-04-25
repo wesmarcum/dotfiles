a44f383805a720acb2bc0d6eaee9cf21c1ac9c57
wes <34384551+wesmarcum@users.noreply.github.com>
2023-04-24 20:05:51 -0500

    fzf; add version check

--- zsh/rc.d/10-fzf.zsh
+++ zsh/rc.d/10-fzf.zsh
@@ -7,6 +7,10 @@
 # Run only if fzf is installed.
 if type fzf > /dev/null 2>&1; then
 
+    # Get fzf version and set required version for custom border.
+    ver_fzf=$(fzf --version | cut -d' ' -f1)
+    ver_req=0.35.0
+
     # Source key bindings and completions for fzf package.
     if [[ $OSTYPE == linux* || $OSTYPE == freebsd* ]]; then
         # Load keys/completion for Linux/FreeBSD
@@ -54,16 +58,27 @@ if type fzf > /dev/null 2>&1; then
         }
     fi
 
-    # Set default fzf options.
-    export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border=rounded --border-label-pos=10 --multi --color='dark,fg+:red'"
-    export FZF_CTRL_T_OPTS="--border-label='| Home Files |'"
-    export FZF_CTRL_R_OPTS="--border-label='| History |'"
+    # Check version numbers.
+    if [[ "$(printf '%s\n' "${ver_req}" "${ver_fzf}" | sort -V | head -n1)" = "${ver_req}" ]]; then
+        # Set custom border.
+        export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border=double --border-label-pos=10 --multi --color='dark,fg+:red'"
+        export FZF_CTRL_T_OPTS="--border-label='| Home Files |'"
+        export FZF_CTRL_R_OPTS="--border-label='| History |'"
 
-    # Use preview with 'tree' if available.
-    if type tree > /dev/null 2>&1; then
-        export FZF_ALT_C_OPTS="--border-label='| Home Directories |' --preview='tree -C {} | head -n 50'"
+        # Use preview with 'tree' if available.
+        if type tree > /dev/null 2>&1; then
+            export FZF_ALT_C_OPTS="--border-label='| Home Directories |' --preview='tree -C {} | head -n 50'"
+        else
+            export FZF_ALT_C_OPTS="--border-label='| Home Directories |'"
+        fi
     else
-        export FZF_ALT_C_OPTS="--border-label='| Home Directories |'"
+        # Set standard options.
+        export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border=rounded --multi --color='dark,fg+:red'"
+
+        # Use preview with 'tree' if available.
+        if type tree > /dev/null 2>&1; then
+            export FZF_ALT_C_OPTS="--preview='tree -C {} | head -n 50'"
+        fi
     fi
 
     # Load fzf functions.
