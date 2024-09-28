da4c9bde1cf78e207e894e893de12c00d74ebd5b
Wes Marcum <34384551+wesmarcum@users.noreply.github.com>
2024-09-28 17:49:24 -0500

    merge maint/fzf_cleanup (#6)
    * fzf; use integration script for version >= 0.48.0, remove old border options
    
    * fzf; set theme to one dark, set marker/pointer

--- zsh/rc.d/10-fzf.zsh
+++ zsh/rc.d/10-fzf.zsh
@@ -7,41 +7,48 @@
 # Run only if fzf is installed.
 if type fzf > /dev/null 2>&1; then
 
-    # Get fzf version and set required version for custom border.
+    # Get fzf version and set required version for integration script.
     ver_fzf=$(fzf --version | cut -d' ' -f1)
-    ver_req=0.35.0
+    ver_req=0.48.0
 
-    # Source key bindings and completions for fzf package.
-    if [[ $OSTYPE == linux* || $OSTYPE == freebsd* ]]; then
-        # Load keys/completion for Linux/FreeBSD
-        if [[ -d /usr/share/doc/fzf/examples ]]; then
-            # Debian.
-            source /usr/share/doc/fzf/examples/key-bindings.zsh
-            source /usr/share/doc/fzf/examples/completion.zsh
-        elif [[ -d /usr/local/share/examples/fzf/shell ]]; then
-            # FreeBSD.
-            source /usr/local/share/examples/fzf/shell/key-bindings.zsh
-            source /usr/local/share/examples/fzf/shell/completion.zsh
-        elif [[ -d /usr/share/fzf ]]; then
-            # Arch.
-            source /usr/share/fzf/key-bindings.zsh
-            source /usr/share/fzf/completion.zsh
-        fi
+    # Check version numbers.
+    if [[ "$(printf '%s\n' "${ver_req}" "${ver_fzf}" | sort -V | head -n1)" = "${ver_req}" ]]; then
+      # Set up fzf key bindings and fuzzy completion
+      # eval "$(fzf --zsh)" 
+      znap eval fzf-shellenv 'fzf --zsh'
+    else
+      # Source key bindings and completions for fzf package.
+      if [[ $OSTYPE == linux* || $OSTYPE == freebsd* ]]; then
+          # Load keys/completion for Linux/FreeBSD
+          if [[ -d /usr/share/doc/fzf/examples ]]; then
+              # Debian.
+              source /usr/share/doc/fzf/examples/key-bindings.zsh
+              source /usr/share/doc/fzf/examples/completion.zsh
+          elif [[ -d /usr/local/share/examples/fzf/shell ]]; then
+              # FreeBSD.
+              source /usr/local/share/examples/fzf/shell/key-bindings.zsh
+              source /usr/local/share/examples/fzf/shell/completion.zsh
+          elif [[ -d /usr/share/fzf ]]; then
+              # Arch.
+              source /usr/share/fzf/key-bindings.zsh
+              source /usr/share/fzf/completion.zsh
+          fi
 
-        # Keys/completion for NixOS.
-        if [[ -n "${commands[fzf-share]}" ]]; then
+          # Keys/completion for NixOS.
+          if [[ -n "${commands[fzf-share]}" ]]; then
           source "$(fzf-share)/key-bindings.zsh"
           source "$(fzf-share)/completion.zsh"
-        fi
-    elif [[ $OSTYPE == darwin* ]]; then
-        if [[ -n "${commands[fzf-share]}" ]]; then
+          fi
+      elif [[ $OSTYPE == darwin* ]]; then
+          if [[ -n "${commands[fzf-share]}" ]]; then
           # nix.
           source "$(fzf-share)/key-bindings.zsh"
           source "$(fzf-share)/completion.zsh"
-        elif [[ -f ~/.config/fzf/fzf.zsh ]]; then
+          elif [[ -f ~/.config/fzf/fzf.zsh ]]; then
           # brew.
           source ~/.config/fzf/fzf.zsh
-        fi
+          fi
+      fi
     fi
 
     # Change the trigger to '++' instead of the default '**'.
@@ -65,27 +72,26 @@ if type fzf > /dev/null 2>&1; then
         }
     fi
 
-    # Check version numbers.
-    if [[ "$(printf '%s\n' "${ver_req}" "${ver_fzf}" | sort -V | head -n1)" = "${ver_req}" ]]; then
-        # Set custom border.
-        export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border=double --border-label-pos=10 --multi --color='dark,fg+:red'"
-        export FZF_CTRL_T_OPTS="--border-label='| Home Files |'"
-        export FZF_CTRL_R_OPTS="--border-label='| History |'"
+    # Set layout and theme.
+    export FZF_DEFAULT_OPTS='
+      --height 50%
+      --layout=reverse
+      --border=double
+      --border-label-pos=10
+      --multi
+      --marker="|" --pointer=">" --separator="─" --scrollbar="│"
+      --color=dark
+      --color=fg:-1,bg:-1,hl:#c678dd,fg+:#ffffff,bg+:#4b5263,hl+:#d858fe
+      --color=info:#98c379,prompt:#61afef,pointer:#be5046,marker:#e5c07b,spinner:#61afef,header:#61afef
+    '
+    export FZF_CTRL_T_OPTS="--border-label='| Home Files |'"
+    export FZF_CTRL_R_OPTS="--border-label='| History |'"
 
-        # Use preview with 'tree' if available.
-        if type tree > /dev/null 2>&1; then
-            export FZF_ALT_C_OPTS="--border-label='| Home Directories |' --preview='tree -C {} | head -n 50'"
-        else
-            export FZF_ALT_C_OPTS="--border-label='| Home Directories |'"
-        fi
+    # Use preview with 'tree' if available.
+    if type tree > /dev/null 2>&1; then
+        export FZF_ALT_C_OPTS="--border-label='| Home Directories |' --preview='tree -C {} | head -n 50'"
     else
-        # Set standard options.
-        export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border=rounded --multi"
-
-        # Use preview with 'tree' if available.
-        if type tree > /dev/null 2>&1; then
-            export FZF_ALT_C_OPTS="--preview='tree -C {} | head -n 50'"
-        fi
+        export FZF_ALT_C_OPTS="--border-label='| Home Directories |'"
     fi
 
     # Load fzf functions.
