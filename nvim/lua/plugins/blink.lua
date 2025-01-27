512452b018712c86e9e57ba0ac498ee2569a9372
wes <34384551+wesmarcum@users.noreply.github.com>
2025-01-27 09:24:29 -0600

    neovim; fix completion option for blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -4,7 +4,7 @@ return {
   "saghen/blink.cmp",
   opts = function(_, opts)
     -- Insert text when selecting.
-    opts.completion = { list = { selection = "auto_insert" } }
+    opts.completion = { list = { selection = { preselect = false, auto_insert = true } } }
     -- opts.keymap = {
     --   preset = "enter",
     --   -- ["<C-y>"] = { "select_and_accept" },
