78c9f16d345870b7a7d95505bdddea17b3d385d4
wes <34384551+wesmarcum@users.noreply.github.com>
2025-05-05 16:48:30 -0500

    neovim; cleanup options file

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,51 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Line wrapping
-vim.opt.wrap = true
-vim.opt.linebreak = true
-
--- Line numbers
-vim.opt.number = true
-vim.opt.relativenumber = false
-
--- Tabs and indents
-vim.opt.tabstop = 8
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.autoindent = true
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-
--- Search
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.incsearch = true
-
--- Undo
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undofile = true
-
--- Better scrolling visibility
-vim.opt.scrolloff = 8
-
--- List characters
-vim.opt.listchars = {
-  space = ".",
-  eol = "$",
-  nbsp = "␣",
-  trail = "-",
-  precedes = "←",
-  extends = "→",
-  tab = "▸-",
-  conceal = "※",
-}
-vim.opt.list = false
-
--- Auto complete
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
 -- Set picker
 -- Can be one of: telescope, fzf
 -- Leave it to "auto" to automatically use the picker
@@ -57,3 +12,56 @@ vim.g.lazyvim_picker = "auto"
 -- Leave it to "auto" to automatically use the completion engine
 -- enabled with `:LazyExtras`
 vim.g.lazyvim_cmp = "auto"
+
+local opt = vim.opt
+
+-- Line wrapping
+opt.wrap = true
+opt.linebreak = true
+
+-- Line numbers
+opt.number = true
+opt.relativenumber = false
+
+-- Tabs and indents
+opt.tabstop = 8
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.autoindent = true
+opt.expandtab = true
+opt.smarttab = true
+
+-- Search
+opt.ignorecase = true
+opt.smartcase = true
+opt.incsearch = true
+
+-- Undo
+opt.swapfile = false
+opt.backup = false
+opt.undofile = true
+
+-- Better scrolling visibility
+opt.scrolloff = 8
+
+-- List characters
+opt.listchars = {
+  space = ".",
+  eol = "$",
+  nbsp = "␣",
+  trail = "-",
+  precedes = "←",
+  extends = "→",
+  tab = "▸-",
+  conceal = "※",
+}
+-- hide list characters by default
+opt.list = false
+
+-- Auto complete
+opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Clipboard
+-- only set clipboard if not in ssh, to make sure the OSC 52
+-- integration works automatically. Requires Neovim >= 0.10.0
+opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
